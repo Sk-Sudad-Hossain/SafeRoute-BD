7968cfdd2041c5f72e0281cbe9584de11acb3746
codewithrezowana <rezowana.mehjabin.lorel@g.bracu.ac.bd>
2026-03-14 02:49:57 +0600

    Map Dashboard and report popup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,6 +5,7 @@ import AdminDashboard from "./pages/AdminDashboard";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import HomePage from "./pages/Homepage";
+import MapTestPage from "./pages/MapTestPage";
 
 export default function App() {
   return (
@@ -12,7 +13,7 @@ export default function App() {
       <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
-
+      <Route path="/map-test" element={<MapTestPage />} />
       <Route
         path="/admin"
         element={
