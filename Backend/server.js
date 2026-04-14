9f5b2360a919cdb63c9f83f610c8edd1dbd6b33a
Spidy_Sain <sk.sudad.hossain@g.bracu.ac.bd>
2026-04-14 14:19:37 +0600

    fix: use env var for socket URL

--- Backend/server.js
+++ Backend/server.js
@@ -21,7 +21,7 @@ export const io = new Server(server, {
   cors: {
     origin: [
               "http://localhost:5173",
-              "https://your-vercel-app.vercel.app"
+              "https://safe-route-bd.vercel.app"
             ],
     methods: ["GET", "POST", "PATCH", "DELETE"],
   },
